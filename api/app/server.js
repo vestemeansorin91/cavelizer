42ae957329e66ed04345805c37f2b2d5ef668c04
sorinvestemean <vestemeansorin@gmail.com>
2022-06-07 21:43:05 +0300

    server with express implemented

--- api/app/server.js
+++ api/app/server.js
@@ -1,14 +1,34 @@
-const http = require("http");
+import express from "express";
+import chalk from "chalk";
 
-const hostname = "127.0.0.1";
-const port = 3333;
+const PORT = 3333;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+app.get("/recodenized", function (req, res) {
+  res.writeHead(200);
+  res.end("Ai ajuns pe reCODEnized");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/api", function (req, res) {
+  res.writeHead(200);
+  res.end("Ai ajuns pe api....");
 });
+
+app.get("*", function (req, res) {
+  res.end(404);
+});
+
+app.listen(PORT, function () {
+  console.log(
+    chalk.bgBlue("Server started: "),
+    chalk.blue(`Listening to port ${PORT}`)
+  );
+});
+
+// LINUX
+// netstat -ano | findstr :3000
+// taskkill /PID 5192 /F
+
+// MACOS
+// lsof -i -P | grep LISTEN | grep :3000
+// kill -9 111111
