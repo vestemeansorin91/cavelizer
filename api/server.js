7544aa8de79645f642114e4c74e6f71106f2db1a
sorinvestemean <vestemeansorin@gmail.com>
2022-06-20 21:40:16 +0300

    api: refactoring + dotenv

--- api/server.js
+++ api/server.js
@@ -1,21 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const PORT = 3333;
 
 app.use(express.json({ limit: "50mb" }));
 
 /* Prepare mongo setup */
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-const url = "mongodb://localhost:27017/cavelizer";
 
 /* Routes */
-const userRoutes = require("./user/user.routes");
+const userRoutes = require("./app/modules/user/user.routes");
 
 app.use("/api/users", userRoutes);
 
-mongoose.connect(url, function (err) {
+mongoose.connect(process.env.MONGO_URL, function (err) {
   if (err) console.log("Mongo error!", err);
   console.log("Database is ready!");
-  app.listen(PORT, () => console.log(`Server started, on port ${PORT}`));
+  app.listen(process.env.PORT, () =>
+    console.log(`Server started, on port ${process.env.PORT}`)
+  );
 });
