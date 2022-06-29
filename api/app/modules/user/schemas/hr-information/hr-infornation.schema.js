5ca1151ed7a2da726a5971c5ddcd02ec3e448780
Alex6665 <alex6665stefan@yahoo.com>
2022-06-29 22:44:31 +0300

    finish weeklyHours

--- api/app/modules/user/schemas/hr-information/hr-infornation.schema.js
+++ api/app/modules/user/schemas/hr-information/hr-infornation.schema.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const hrInformationSchema = mongoose.Schema({
-  birthDate: Date
+  birthDate: Date,
+  weeklyHours: Number
 });
 
 module.exports = hrInformationSchema;
