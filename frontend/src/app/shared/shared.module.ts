a95ae8f7f747f0f538219552d9e96149d9e57203
sorinvestemean <vestemeansorin@gmail.com>
2022-06-30 21:34:09 +0300

    fe: created new button on angular

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -7,8 +7,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { ButtonComponent } from './components/button/button.component';
 
-const COMPONENTS: any[] = [PageNotFoundComponent];
+const COMPONENTS: any[] = [PageNotFoundComponent, ButtonComponent];
 
 const SERVICES: any[] = [];
 
