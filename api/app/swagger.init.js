a7fd5337a4fbef493c072546d140751a5bf4107c
sorinvestemean <vestemeansorin@gmail.com>
2022-07-01 11:49:35 +0300

    be: swagger improvments

--- api/app/swagger.init.js
+++ api/app/swagger.init.js
@@ -1,11 +1,14 @@
-const SWAGGER_OUTPUT_PATH = './config/swagger/swagger-output.json';
+const SWAGGER_OUTPUT_PATH = 'app/config/swagger/swagger-output.json';
+const fs = require('fs');
 
-exports.initSwagger = app => {
+exports.initSwagger = async app => {
   try {
-    const swaggerFile = require(SWAGGER_OUTPUT_PATH);
-    const swaggerUi = require('swagger-ui-express');
-    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+    await fs.promises.access(SWAGGER_OUTPUT_PATH, fs.constants.F_OK, () => {
+      const swaggerFile = require(SWAGGER_OUTPUT_PATH);
+      const swaggerUi = require('swagger-ui-express');
+      app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+    });
   } catch (e) {
-    console.log(e);
+    console.info(`No swagger-output.json file generated. You can run npm run swagger to generate it!`);
   }
 };
