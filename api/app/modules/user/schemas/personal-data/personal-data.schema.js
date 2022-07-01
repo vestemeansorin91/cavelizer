2c0eb47efb212728791900ea3c56e497be15a43d
Varzaru <alvarzaru@contractor.ea.com>
2022-07-02 01:28:34 +0300

    added city property and enum to Schema

--- api/app/modules/user/schemas/personal-data/personal-data.schema.js
+++ api/app/modules/user/schemas/personal-data/personal-data.schema.js
@@ -1,6 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const CityEnum = require('../../../../shared/enums/city.enum');
+
 const personalDataSchema = mongoose.Schema({
   street: String,
+  city: {
+    type: String,
+    enum: CityEnum
+  }
 });
 
 module.exports = personalDataSchema;
