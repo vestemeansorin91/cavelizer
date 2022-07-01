fb026abd37dc7d062b92f41f32d4d62bdb355697
Varzaru <alvarzaru@contractor.ea.com>
2022-07-02 01:18:16 +0300

    added zipCode property to personalDataSchema

--- api/app/modules/user/schemas/personal-data/personal-data.schema.js
+++ api/app/modules/user/schemas/personal-data/personal-data.schema.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const personalDataSchema = mongoose.Schema({
   street: String,
+  zipCode: String
 });
 
 module.exports = personalDataSchema;
