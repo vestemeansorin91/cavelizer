a9652f8b9c56dad059e226e2958228464aa72ab7
Varzaru <alvarzaru@contractor.ea.com>
2022-07-02 01:05:12 +0300

    added houseNumber into personalDataSchema

--- api/app/modules/user/schemas/personal-data/personal-data.schema.js
+++ api/app/modules/user/schemas/personal-data/personal-data.schema.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const personalDataSchema = mongoose.Schema({
   street: String,
+  houseNumber: Number
 });
 
 module.exports = personalDataSchema;
