7a37e3c43de50dd2d8e09c2357a7fc1c63c0d659
Varzaru <alvarzaru@contractor.ea.com>
2022-07-02 01:36:13 +0300

    added phoneNumber property to Schema

--- api/app/modules/user/schemas/personal-data/personal-data.schema.js
+++ api/app/modules/user/schemas/personal-data/personal-data.schema.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const personalDataSchema = mongoose.Schema({
   street: String,
+  phoneNumber: String
 });
 
 module.exports = personalDataSchema;
