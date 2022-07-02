1b49388e3fe8db00d5c87b373682092d4e64c597
sorinvestemean <vestemeansorin@gmail.com>
2022-07-02 13:03:50 +0300

    fe: admin topbar + sidenav improvments / changed to black

---
+++ frontend/src/app/modules/dashboard/components/topbar/topbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Store} from "@ngrx/store";
+import {Observable} from "rxjs";
+import {JwtPayloadInterface} from "../../../../shared/types/jwt-payload.interface";
+import {StoreStateInterface} from "../../../../store";
+import {getUserSelector} from "../../../auth/store/auth.selectors";
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.scss']
+})
+export class TopbarComponent implements OnInit {
+  public currentUser$: Observable<JwtPayloadInterface | null> = this.store.select(getUserSelector);
+
+  constructor(private store: Store<StoreStateInterface>) {}
+
+  ngOnInit(): void {
+  }
+
+}
