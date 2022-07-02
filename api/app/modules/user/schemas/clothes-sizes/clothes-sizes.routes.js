fb0cdfa4b4e7978ea46989d817346a0ef2220fe4
sorinvestemean <vestemeansorin@gmail.com>
2022-07-02 09:42:46 +0300

    be: fixed isAuthenticatedMiddleware path + fe: added breadcrumb component

--- api/app/modules/user/schemas/clothes-sizes/clothes-sizes.routes.js
+++ api/app/modules/user/schemas/clothes-sizes/clothes-sizes.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ClothesSizesController = require('./clothes-sizes.controller');
 
-const AuthenticatedMiddleware = require('../../../../shared/middlewares/passport/passport.middleware');
+const AuthenticatedMiddleware = require('../../../../shared/middlewares/is-authenticated.middleware');
 
 router.patch('/users/:userId/updateClothesSizes', AuthenticatedMiddleware, ClothesSizesController.updateClothesSizes);
 
