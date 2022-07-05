4e0bf4029318bdc09acb5090324d830f9c9beddf
sorinvestemean <vestemeansorin@gmail.com>
2022-07-05 21:20:40 +0300

    be: fixed profile validator

--- frontend/src/app/shared/pipes/default-image.pipe.ts
+++ frontend/src/app/shared/pipes/default-image.pipe.ts
@@ -1,10 +1,16 @@
 import { Pipe, PipeTransform } from "@angular/core";
+import {environment} from "../../../environments/environment";
 
 @Pipe({
   name: "defaultImage"
 })
 export class DefaultImagePipe implements PipeTransform {
-  transform(value: string | null | undefined): string {
-    return !!value ? value : 'https://media.istockphoto.com/vectors/profile-placeholder-image-gray-silhouette-no-photo-vector-id1320751178?k=20&m=1320751178&s=612x612&w=0&h=qBg0zl3GQvll4dI5m0VDidziZN5VWnghNQ2rt-6aln0=';
+  transform(avatarBlobUrl: string | null | undefined): string {
+    if(!!avatarBlobUrl) {
+      const prefix = environment.baseUrl;
+      return  prefix + "/" + avatarBlobUrl;
+    }
+
+    return '/assets/default-avatar.jpeg';
   }
 }
