6a7220c39c1fc37580f27d2d99d33ff17de9f7f8
Alex6665 <alex6665stefan@yahoo.com>
2022-07-07 20:49:03 +0300

    implemented Shop Headline

--- frontend/src/app/modules/shop/shop.module.ts
+++ frontend/src/app/modules/shop/shop.module.ts
@@ -1,15 +1,15 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {SharedModule} from 'src/app/shared/shared.module';
-import {ShopMenuComponent} from "./components/shop-menu/shop-menu.component";
-import {ShopComponent} from './shop.component';
-import {ShopRoutes} from './shop.routes';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ShopHeadlineComponent } from './components/shop-headline/shop-headline.component';
+import { ShopMenuComponent } from './components/shop-menu/shop-menu.component';
+import { ShopComponent } from './shop.component';
+import { ShopRoutes } from './shop.routes';
 
-const COMPONENTS = [ShopComponent, ShopMenuComponent];
+const COMPONENTS = [ShopComponent, ShopHeadlineComponent, ShopMenuComponent];
 
 @NgModule({
   declarations: [COMPONENTS],
   imports: [CommonModule, ShopRoutes, SharedModule]
 })
-export class ShopModule {
-}
+export class ShopModule {}
