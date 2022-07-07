e4c1bf7b53269854552e0cedc86bf769fdd9bd37
sorinvestemean <vestemeansorin@gmail.com>
2022-07-07 21:04:04 +0300

    be: emergency contact - fixed

--- api/app/modules/user/schemas/emergency-contact/emergency-contact.routes.js
+++ api/app/modules/user/schemas/emergency-contact/emergency-contact.routes.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
 const isAuthenticatedMiddleware = require('../../../../shared/middlewares/is-authenticated.middleware');
-const EmegencyContactController = require('./emergency-contact.controller');
+const EmergencyContactController = require('./emergency-contact.controller');
 
-
-const isAuthenticatedMiddleware = passport.authenticate('jwt', {
-  session: false
-});
-
-
-router.patch('/users/:userId/updateEmergencyContact', isAuthenticatedMiddleware, EmegencyContactController.updateEmergencyContact);
+router.patch('/users/:userId/updateEmergencyContact', isAuthenticatedMiddleware, EmergencyContactController.updateEmergencyContact);
 
 module.exports = router;
--- api/app/modules/user/schemas/emergency-contact/emergency-contact.schema.js
+++ api/app/modules/user/schemas/emergency-contact/emergency-contact.schema.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
 const RelationshipEnum = require("../../../../shared/enums/relationship.enum");
+
 const emergencyContactSchema = mongoose.Schema({
-  fullName: String,
-  phoneNumber: String,
-  relationshipType: {
-    type: String,
-    enum:RelationshipEnum
-  },
+    fullName: String,
+    phoneNumber: String,
+    relationshipType: {
+        type: String,
+        enum: RelationshipEnum
+    },
 });
 
 module.exports = emergencyContactSchema; 
