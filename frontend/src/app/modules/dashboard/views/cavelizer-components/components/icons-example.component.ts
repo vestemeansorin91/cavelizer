32c8b815551108e6602284eb7da5ca6e6f850361
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2022-07-09 22:27:59 +0300

    icons fonts

--- frontend/src/app/modules/dashboard/views/cavelizer-components/components/icons-example.component.ts
+++ frontend/src/app/modules/dashboard/views/cavelizer-components/components/icons-example.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {projectIcons} from "../../../../../shared/components/icon/project-icons";
+import { Component } from '@angular/core';
+import { projectIcons } from '../../../../../shared/components/icon/project-icons';
 
 @Component({
   selector: 'app-icons-example',
@@ -8,18 +8,14 @@ import {projectIcons} from "../../../../../shared/components/icon/project-icons"
       <mat-panel-title>
         <strong>cavelizer-icon</strong>
       </mat-panel-title>
-      <mat-panel-description>
-        Creat de Sorin
-      </mat-panel-description>
+      <mat-panel-description> Creat de Sorin </mat-panel-description>
     </mat-expansion-panel-header>
 
-
-    <div class="flex gap--60">
+    <div class="flex gap--60 flex--wrap">
       <cavelizer-icon *ngFor="let icon of projectIcons" [name]="icon" class="size--24"></cavelizer-icon>
     </div>
-
-  </mat-expansion-panel>`,
+  </mat-expansion-panel>`
 })
-export class IconsExampleComponent{
+export class IconsExampleComponent {
   public projectIcons = Object.keys(projectIcons);
 }
