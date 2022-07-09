ecb7f2c94de68151a9397fd921a625fa3e5c14db
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2022-07-09 12:42:49 +0300

    implemented props from figma , and add some helpers for responsive design , classes and breakpoints

---
+++ frontend/src/app/modules/shop/components/shop-header/shop-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'; 
+
+@Component({
+    selector: 'app-shop-header',
+    templateUrl: './shop-header.component.html',
+    styleUrls: ['./shop-header.component.scss']
+})
+
+export class ShopHeaderComponent{}
