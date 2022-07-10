77b555974dcdccf967a201692112405c440bd1dd
Sorin Vestemean <vestemeansorin91@gmail.com>
2022-07-10 14:03:17 +0300

    implemented form on public profile

--- frontend/src/app/modules/profile/components/user-profile/user-profile.component.ts
+++ frontend/src/app/modules/profile/components/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-user-profile',
@@ -6,7 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
-  constructor() {}
+  public userPublicProfileFormGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.buildFormGroup();
+  }
 
   ngOnInit(): void {}
+
+
+  private buildFormGroup() {
+    this.userPublicProfileFormGroup = this.fb.group({
+      office: this.fb.control(''),
+      department: this.fb.control('development'),
+      position: this.fb.control('junior'),
+      team: this.fb.control('qa'),
+      phone: this.fb.control('')
+    })
+  }
 }
+
+
+// Etapa de build formular
+
+// Etapa .... optionala .. de patch
