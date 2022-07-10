82ba48d2b3709673b643252d961a83b9358b30cd
Sorin Vestemean <vestemeansorin91@gmail.com>
2022-07-10 14:53:31 +0300

    fe: cleaning

--- frontend/src/app/modules/profile/components/user-profile/user-profile.component.ts
+++ frontend/src/app/modules/profile/components/user-profile/user-profile.component.ts
@@ -6,7 +6,6 @@ import {ProfileService} from "../../profile.service";
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent {
   public userPublicProfileFormGroup: FormGroup;
