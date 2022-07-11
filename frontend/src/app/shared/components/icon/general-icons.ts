7d7bd0da3c2a08fa7786504251cedc2f9ba06bee
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2022-07-11 11:17:50 +0300

    last modifications

--- frontend/src/app/shared/components/icon/general-icons.ts
+++ frontend/src/app/shared/components/icon/general-icons.ts
@@ -1,1 +1,15 @@
-export const generalIcons = ['arrow-left', 'arrow-right', 'users'];
+export const generalIcons = [
+  'arrow-left',
+  'arrow-down',
+  'arrow-right',
+  'arrow-top-bottom',
+  'basket',
+  'check',
+  'compare',
+  'heart',
+  'users',
+  'mail',
+  'search',
+  'star-empty',
+  'star-full'
+];
