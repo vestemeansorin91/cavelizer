db0e05526ce5237d82b059a78917dfc4dca91059
mihaifirst <varzaru.alexmihai@gmail.com>
2022-07-20 01:53:30 +0300

    finished clothes sizes form

--- frontend/src/app/modules/dashboard/views/profile/profile.service.ts
+++ frontend/src/app/modules/dashboard/views/profile/profile.service.ts
@@ -1,16 +1,15 @@
-import {HttpClient} from "@angular/common/http";
-import {Injectable} from "@angular/core";
-import {Observable} from "rxjs";
-import {environment} from "../../../../../environments/environment";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../../environments/environment';
 
 const BASE_URL = environment.apiUrl + '/users';
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ProfileService {
   public profile: any = null;
   public userId = '';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   public uploadAvatar(fileToUpload: File | any): Observable<any> {
     const formData: FormData = new FormData();
@@ -21,11 +20,15 @@ export class ProfileService {
   /* This is to get all user profile */
   public getUserByIdWithProfile(userId: string) {
     this.userId = userId;
-    return this.http.get(`${BASE_URL}/${userId}/getUserByIdWithProfile`)
+    return this.http.get(`${BASE_URL}/${userId}/getUserByIdWithProfile`);
   }
 
   /* This you should implement for bank details, clothes sizes, emergency contact etc */
-  public saveUserPublicProfile(payload:any) {
-    return this.http.patch(`${BASE_URL}/${this.userId}/updatePublicProfile`, payload)
+  public saveUserPublicProfile(payload: any) {
+    return this.http.patch(`${BASE_URL}/${this.userId}/updatePublicProfile`, payload);
+  }
+
+  public saveUserClothesSizes(payload: any) {
+    return this.http.patch(`${BASE_URL}/${this.userId}/updateClothesSizes`, payload);
   }
 }
