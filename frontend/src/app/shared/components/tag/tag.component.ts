a9dbb48ff41501ea3f6279b2cb9f5febb3a385ad
mihaifirst <varzaru.alexmihai@gmail.com>
2022-07-25 18:34:15 +0300

    finished tag component

---
+++ frontend/src/app/shared/components/tag/tag.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cavelizer-tag',
+  templateUrl: './tag.component.html',
+  styleUrls: ['./tag.component.scss']
+})
+export class TagComponent {}
