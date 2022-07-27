d66dd894e099637c32cc2fda17fe0b8b2d3f7af5
mihaifirst <varzaru.alexmihai@gmail.com>
2022-07-27 20:46:51 +0300

    finished hr information

--- frontend/src/app/modules/dashboard/views/profile/components/user-hr-information/user-hr-information.component.ts
+++ frontend/src/app/modules/dashboard/views/profile/components/user-hr-information/user-hr-information.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NotificationsService } from 'angular2-notifications';
+import { ProfileService } from '../../profile.service';
 
 @Component({
   selector: 'app-user-hr-information',
@@ -6,4 +9,47 @@ import {Component} from '@angular/core';
   styleUrls: ['./user-hr-information.component.scss']
 })
 export class UserHrInformationComponent {
+  public userHrInformationFormGroup: FormGroup;
+
+  constructor(private fb: FormBuilder, private profileService: ProfileService, private notificationsService: NotificationsService) {
+    this.buildFormGroup();
+    this.patchFormGroup();
+  }
+
+  ngOnInit(): void {}
+
+  public onSaveHrInformation() {
+    this.profileService.saveUserHrInformation(this.userHrInformationFormGroup.value).subscribe(() => {
+      this.notificationsService.success('Success', 'User Clothes Sizes saved!');
+    });
+  }
+
+  private buildFormGroup() {
+    this.userHrInformationFormGroup = this.fb.group({
+      birthDate: this.fb.control(''),
+      employmentType: this.fb.control(''),
+      status: this.fb.control(''),
+      nationality: this.fb.control(''),
+      weeklyHours: this.fb.control(''),
+      gender: this.fb.control('')
+    });
+  }
+
+  private patchFormGroup() {
+    const { hrInformation } = this.profileService.profile;
+
+    if (!hrInformation) {
+      return;
+    }
+
+    this.userHrInformationFormGroup.patchValue({
+      birthDate: hrInformation.birthDate,
+      employmentType: hrInformation.employmentType,
+      status: hrInformation.status,
+      nationality: hrInformation.nationality,
+      weeklyHours: hrInformation.weeklyHours,
+      gender: hrInformation.gender,
+      footSize: hrInformation.footSize
+    });
+  }
 }
