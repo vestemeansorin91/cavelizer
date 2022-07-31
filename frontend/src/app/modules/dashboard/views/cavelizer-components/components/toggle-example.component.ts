b4bcb6e3c7bbad6e560b843451429797cd405b6e
mihaifirst <varzaru.alexmihai@gmail.com>
2022-08-01 02:28:50 +0300

    finished toggle component

--- frontend/src/app/modules/dashboard/views/cavelizer-components/components/toggle-example.component.ts
+++ frontend/src/app/modules/dashboard/views/cavelizer-components/components/toggle-example.component.ts
@@ -13,23 +13,24 @@ import { Component } from '@angular/core';
     <div class="flex--column gap--16 mb--16">
       <cvz-toggle [(ngModel)]="leftOrRight">Activate some option</cvz-toggle>
       <cvz-toggle [(ngModel)]="leftOrRight2">Activate some option</cvz-toggle>
-      <cvz-toggle [(ngModel)]="leftOrRightDisabled" [disabled]="true">This one is disabled</cvz-toggle>
-      <cvz-toggle [(ngModel)]="leftOrRightDisabledTrue" [disabled]="true">This one is disabled</cvz-toggle>
+      <cvz-toggle [(ngModel)]="leftOrRightDisabled" [disabled]="true">This one is disabled and off</cvz-toggle>
+      <cvz-toggle [(ngModel)]="leftOrRightDisabledTrue" [disabled]="true">This one is disabled and on</cvz-toggle>
     </div>
     <div class="flex--column gap--16">
       <cvz-toggle class="admin" [(ngModel)]="correct">Activate some option</cvz-toggle>
-      <cvz-toggle class="admin" [(ngModel)]="leftOrRight2">Activate some option</cvz-toggle>
-      <cvz-toggle class="admin" [(ngModel)]="leftOrRightDisabled" [disabled]="true">This one is disabled</cvz-toggle>
-      <cvz-toggle class="admin" [(ngModel)]="leftOrRightDisabledTrue" [disabled]="true"
-        >This one is disabled</cvz-toggle
-      >
+      <cvz-toggle class="admin" [(ngModel)]="correct2">Activate some option</cvz-toggle>
+      <cvz-toggle class="admin" [(ngModel)]="disabledfalse" [disabled]="true">This one is disabled and off</cvz-toggle>
+      <cvz-toggle class="admin" [(ngModel)]="disabledtrue" [disabled]="true">This one is disabled and on</cvz-toggle>
     </div>
   </mat-expansion-panel>`
 })
 export class ToggleExampleComponent {
-  correct = true;
   leftOrRight = true;
   leftOrRight2 = true;
   leftOrRightDisabled = false;
   leftOrRightDisabledTrue = true;
+  correct = true;
+  correct2 = true;
+  disabledfalse = false;
+  disabledtrue = true;
 }
