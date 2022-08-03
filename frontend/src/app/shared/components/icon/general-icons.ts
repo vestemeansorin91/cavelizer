c75cae2ddf267a3087a5eb5701d7655bc5e725e5
TheRankN1 <petrutpoenaru@yahoo.com>
2022-08-03 10:30:24 +0300

    added half of icons

--- frontend/src/app/shared/components/icon/general-icons.ts
+++ frontend/src/app/shared/components/icon/general-icons.ts
@@ -1,1 +1,2 @@
-export const generalIcons = ['arrow-left', 'arrow-down', 'arrow-right', 'arrow-top-bottom', 'basket', 'check', 'compare', 'heart', 'users', 'mail', 'search', 'star-empty', 'star-full', 'close'];
+export const generalIcons = ['arrow-left', 'arrow-down', 'arrow-right', 'arrow-top-bottom', 'basket', 'check',
+  'compare', 'heart', 'users', 'mail', 'search', 'star-empty', 'star-full', 'close','tag','settings','permission','product','logout','category','dashboard','user'];
