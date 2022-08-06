42a170cad1d5a1d49d5e7649870504c23c20ea7b
Sorin Vestemean <vestemeansorin91@gmail.com>
2022-08-06 16:01:47 +0300

    typography example text improved

--- frontend/src/app/modules/dashboard/views/cavelizer-components/components/typography-example.ts
+++ frontend/src/app/modules/dashboard/views/cavelizer-components/components/typography-example.ts
@@ -9,12 +9,12 @@ import { Component } from '@angular/core';
       </mat-panel-title>
       <mat-panel-description> Creat de Petrut</mat-panel-description>
     </mat-expansion-panel-header>
-    <h1>22222</h1>
-    <h2>22222</h2>
-    <h3>22222</h3>
-    <h4>22222</h4>
-    <h5>22222</h5>
-    <h6>22222</h6>
+    <h1 [innerText]="'This is a heading <h1>'"></h1>
+    <h2 [innerText]="'This is a heading <h2>'"></h2>
+    <h3 [innerText]="'This is a heading <h3>'"></h3>
+    <h4 [innerText]="'This is a heading <h4>'"></h4>
+    <h5 [innerText]="'This is a heading <h5>'"></h5>
+    <h6 [innerText]="'This is a heading <h6>'"></h6>
   </mat-expansion-panel>`
 })
 export class TypographyExample {}
