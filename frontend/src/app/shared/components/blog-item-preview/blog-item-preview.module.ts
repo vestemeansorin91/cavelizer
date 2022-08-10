66344825b387689ff315072bc3c982b39805a33a
mihaifirst <varzaru.alexmihai@gmail.com>
2022-08-11 01:45:52 +0300

    finished blog-item-preview

--- frontend/src/app/shared/components/blog-item-preview/blog-item-preview.module.ts
+++ frontend/src/app/shared/components/blog-item-preview/blog-item-preview.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {BlogPreviewComponent} from './blog-item-preview.component';
+import {TagModule} from "../tag/tag.module";
+
 
 @NgModule({
   declarations: [BlogPreviewComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TagModule],
   exports: [BlogPreviewComponent]
 })
 export class BlogPreviewModule {}
