6b6f2a7875b0ad949ec8128f6ec5f2605d4ae1e4
Sorin Vestemean <vestemeansorin91@gmail.com>
2022-08-11 09:02:37 +0300

    heroku

--- api/server.js
+++ api/server.js
@@ -11,18 +11,17 @@ app.use(require('cors')());
 app.use(require('body-parser').json());
 app.use(require('morgan')('tiny'));
 require('./app/swagger.init')
-    .initSwagger(app)
-    .then(_ => null);
+  .initSwagger(app)
+  .then(_ => null);
 
 /* Chat connection / sockets */
 const _ = require('lodash');
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
-const {User} = require('./app/shared/helpers/user.class');
+const { User } = require('./app/shared/helpers/user.class');
 require('./app/sockets/streams')(io, User, _);
 require('./app/sockets/private')(io);
-// require('events').EventEmitter.defaultMaxListeners = 20;
 
 /* Routes */
 const authRoutes = require('./app/modules/auth/auth.routes');
@@ -44,8 +43,8 @@ app.use('/public/assets', express.static(process.env.PWD + '/public/assets'));
 
 const mongoConfig = require('./app/config/mongo.config');
 const srv = err => {
-    if (err) console.log('Mongo error!', err);
-    console.log(`Database is ready! on ${process.env.MONGO_URL}`);
-    server.listen(process.env.PORT, () => console.log(`Server started, on port ${process.env.PORT}`));
+  if (err) console.log('Mongo error!', err);
+  console.log(`Database is ready! on ${process.env.MONGO_URL}`);
+  server.listen(process.env.PORT, () => console.log(`Server started, on ${process.env.PORT}`));
 };
 mongoose.connect(process.env.MONGO_URL, mongoConfig, srv);
