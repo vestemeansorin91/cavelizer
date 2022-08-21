ef3dfdcc8c7c4027bbac50566f02244d3a2cddf7
mihaifirst <varzaru.alexmihai@gmail.com>
2022-08-21 22:37:09 +0300

    modified tag classes and added new icon classes

--- frontend/src/app/modules/dashboard/views/cavelizer-components/components/tag-example.ts
+++ frontend/src/app/modules/dashboard/views/cavelizer-components/components/tag-example.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tag-example',
-  template: `<mat-expansion-panel>
+  template: `<mat-expansion-panel expanded="true">
     <mat-expansion-panel-header>
       <mat-panel-title>
         <strong>cavelizer-tag</strong>
@@ -10,22 +10,34 @@ import { Component } from '@angular/core';
       <mat-panel-description> Creat de Mihai</mat-panel-description>
     </mat-expansion-panel-header>
     <div class="flex flex--wrap gap--16 mb--16">
-      <cvz-tag class="narrow">Tag</cvz-tag>
-      <cvz-tag class="extended">Tag</cvz-tag>
-      <cvz-tag class="fat">Tag</cvz-tag>
-      <cvz-tag class="fat extended">Tag</cvz-tag>
+      <cvz-tag class="narrow">Vegetables</cvz-tag>
+      <cvz-tag class="narrow"
+        >Vegetables<cvz-icon name="close" class="very-small color-primary align--center ml--6"></cvz-icon
+      ></cvz-tag>
+      <cvz-tag class="fat">Vegetables</cvz-tag>
+      <cvz-tag class="fat extended"
+        >Vegetables<cvz-icon name="close" class="very-small color-primary align--center ml--6"></cvz-icon
+      ></cvz-tag>
     </div>
     <div class="flex flex--wrap gap--16 mb--16">
-      <cvz-tag class="disabled narrow ">Tag</cvz-tag>
-      <cvz-tag class="disabled extended ">Tag</cvz-tag>
-      <cvz-tag class="disabled fat">Tag</cvz-tag>
-      <cvz-tag class="disabled fat extended">Tag</cvz-tag>
+      <cvz-tag class="disabled narrow ">Vegetables</cvz-tag>
+      <cvz-tag class="disabled narrow "
+        >Vegetables<cvz-icon name="close" class="very-small color-grey align--center ml--6"></cvz-icon
+      ></cvz-tag>
+      <cvz-tag class="disabled fat">Vegetables</cvz-tag>
+      <cvz-tag class="disabled fat"
+        >Vegetables<cvz-icon name="close" class="very-small color-grey align--center ml--6"></cvz-icon
+      ></cvz-tag>
     </div>
     <div class="flex flex--wrap gap--16 mb--16">
-      <cvz-tag class="default narrow ">Tag</cvz-tag>
-      <cvz-tag class="default extended ">Tag</cvz-tag>
-      <cvz-tag class="default fat">Tag</cvz-tag>
-      <cvz-tag class="default fat extended">Tag</cvz-tag>
+      <cvz-tag class="default narrow ">Vegetables</cvz-tag>
+      <cvz-tag class="default narrow "
+        >Vegetables<cvz-icon name="close" class="very-small color-grey align--center ml--6"></cvz-icon
+      ></cvz-tag>
+      <cvz-tag class="default fat">Vegetables</cvz-tag>
+      <cvz-tag class="default fat "
+        >Vegetables<cvz-icon name="close" class="very-small color-grey align--center ml--6"></cvz-icon
+      ></cvz-tag>
     </div>
   </mat-expansion-panel>`
 })
