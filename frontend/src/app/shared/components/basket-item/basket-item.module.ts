7275be2387b9627edfc14fc4a5cf6b3aeea73960
TheRankN1 <petrutpoenaru@yahoo.com>
2022-08-24 22:13:14 +0300

    resolve comments

--- frontend/src/app/shared/components/basket-item/basket-item.module.ts
+++ frontend/src/app/shared/components/basket-item/basket-item.module.ts
@@ -1,18 +1,18 @@
-import { CommonModule } from '@angular/common';
+import {CommonModule, CurrencyPipe} from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BasketItemComponent } from './basket-item.component';
 import {IconModule} from "../icon/icon.module";
 import {DefaultImagePipeModule} from "../../pipes";
-
+import {LOCALE_ID} from '@angular/core';
 @NgModule({
   declarations: [BasketItemComponent],
   imports: [
     /* Angular Modules */
     CommonModule,
     IconModule,
-    DefaultImagePipeModule
     /* Angular Material Modules */
     /* Cavelizer Modules */
+    DefaultImagePipeModule,
   ],
   exports: [BasketItemComponent]
 })
